ef58baef03036e786f94add2449ed5d24fa38fa0
Vyacheslav Kamenev <vy.kamenev@gmail.com>
2018-07-19 22:51:38 +0300

    set up env

--- app/server.js
+++ app/server.js
@@ -1,12 +1,10 @@
 import app from './app';
-
-const PORT = process.env.PORT || 4000;
+import { PORT } from './config';
 
 const server = app.listen(PORT, (err) => {
   if (err) console.log(err);
 
   console.log(`Server running on port: ${PORT}`);
 });
-
 
 export default server;
