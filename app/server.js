2fcd7fa1a65210200e9a2cd5b8d48b941e4589ad
Vyacheslav Kamenev <vy.kamenev@gmail.com>
2018-07-19 22:45:24 +0300

    set up koa server

---
+++ app/server.js
@@ -0,0 +1,12 @@
+import app from './app';
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, (err) => {
+  if (err) console.log(err);
+
+  console.log(`Server running on port: ${PORT}`);
+});
+
+
+export default server;
