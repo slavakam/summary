f81be06b62791c3167b53ba84a9d4956808f6ff1
Vyacheslav Kamenev <vy.kamenev@gmail.com>
2018-07-19 23:40:22 +0300

    creating summaty

---
+++ app/modules/summaries/models/summary.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from 'mongoose';
+
+const SummarySchema = new Schema({
+  userId: {
+    type: String,
+    required: 'User is required',
+  },
+  title: {
+    type: String,
+    required: 'Title is required',
+    trim: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+  },
+  skype: {
+    type: String,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    // required: 'Tags are required',
+    // trim: true,
+  },
+  description: {
+    type: String,
+    required: 'Description is required',
+    trim: true,
+  },
+  history: [{
+    companyName: {
+      type: String,
+      required: 'Company name is required',
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: 'Title is required',
+      trim: true,
+    },
+    date: {
+      start: {
+        type: Date,
+        required: 'Start date is required',
+      },
+      end: {
+        type: Date,
+      },
+    },
+    currentWork: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      required: 'Description is required',
+      trim: true,
+    },
+  }],
+});
+
+SummarySchema.statics.createFields = ['title', 'phone', 'skype', 'description', 'tags', 'history'];
+
+export default mongoose.model('summary', SummarySchema);
