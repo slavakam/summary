7e2d33e1180b589bce8aad836e419f3b06f02c2d
Vyacheslav Kamenev <vy.kamenev@gmail.com>
2018-07-19 23:00:57 +0300

    mongo connection

---
+++ app/connectors/mongoose-connector.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+mongoose.Promise = Promise;
+
+export default (mongoUri) => {
+  if (!mongoUri) {
+    throw Error('Mongo uri is indefined');
+  }
+
+  return mongoose
+    .connect(mongoUri, { useNewUrlParser: true })
+    .then((mongodb) => {
+      console.log('Mongo connected');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
